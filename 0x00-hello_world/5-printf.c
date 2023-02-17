e6a205708f9e49bf610b3085c48d12244aed27bf
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-02-16 17:10:17 -0800

    Work

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - printing our string
+ *
+ * Description: using the main function
+ * this program prints proper gramma
+ * Return:0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
