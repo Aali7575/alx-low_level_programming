dab78fd906e421cfe79ff1326dfb625254961433
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-02-20 01:45:17 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print if the number is positive nevgative or zeri
+ *
+ * Description: using the main function
+ * this program prints "programming is positive,negative or zero
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 9; c++)
+				
+				if (c > b; &&b > a)
+				{
+					putchar(a + '0');
+					 putchar(b + '0');
+					  putchar(c + '0');
+					  if (a != 7 || b != 8 || c != 9)
+					  {
+						  putchar(',');
+							  putchar(',');
+					  }
+				}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
+
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print if the number is positive nevgative or zeri
+ *
+ * Description: using the main function
+ * this program prints "programming is positive,negative or zero
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar(i / 10 +n'0');
+			putchar(i / 10 +n'0');
+			putchar(i / 10 +n'0');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
