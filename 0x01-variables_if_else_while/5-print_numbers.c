5c42148f6033c5eaacb4311befa92e86955d0d94
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-02-20 00:05:57 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print if the number is positive nevgative or zeri
+ *
+ * Description: using the main function
+ * this program prints "programming is positive,negative or zero
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10.; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
