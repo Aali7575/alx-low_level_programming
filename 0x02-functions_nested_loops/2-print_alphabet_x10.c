920a1e79e7ccc7cde5feb20a3746ef4fc947675d
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-02-21 17:12:20 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ *
+ * Description: Alphabet is printed with a for loop. Process
+ * is repeated ten times with while loop.
+ * Return: 0 on success
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	i = 0;
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		i++;
+		_putchar('\n');
+	}
+}
