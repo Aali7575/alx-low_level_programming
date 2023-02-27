73a605f6411ac711af90bee21529a885fb00a171
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-02-27 15:18:33 -0800

    Task zero

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,6 +6,7 @@
  *  and updates the value it points to to 98.
  *  Return: Nothinig
  *   reset_to_98 - Takes a pointer to an int and updates
+ *    the value it points to 98
  *   @n: An integer input pointer
  */
 void reset_to_98(int *n)
