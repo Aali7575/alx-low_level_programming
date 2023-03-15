f7bde9e2924feed0b7f8c595168cee47dc24e6ac
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-03-15 13:32:40 -0700

    task 2,3 and 4

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,7 +10,12 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s", argv[]);
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 
 	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,18 +1,25 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - multiplies two number
- * @z: argument count
- * @y: character of pointers
- * Return: 0
+ * main - multiply 2 numbers passed to main, or Error
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 if error, 0 if function runs correctly
  */
 
-int main(int z, char *argv y[])
+int main(int argc, char *argv[])
 {
-	atoi(y) = ((int)a * (int)b);
+	(void) argc;
 
-	printf("%d\n", y);
-
-	return (0);
+	if (argv[1] && argv[2])
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+		printf("Error\n");
+	return (1);
 }
+
