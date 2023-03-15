3bfb87d3a869694c4ecf32263c638dc13fd2a79c
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-03-15 07:05:17 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiplies two number
+ * @z: argument count
+ * @y: character of pointers
+ * Return: 0
+ */
+
+int main(int z, char *argv y[])
+{
+	atoi(y) = ((int)a * (int)b);
+
+	printf("%d\n", y);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/practise.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Just need to practise so l can get better
+ * program has to print a new line as well
+ * @argc: argument count
+ * @argv: arrays what what lol
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+		printf("%d", argc);
+
+		return (0);
+
+		}
