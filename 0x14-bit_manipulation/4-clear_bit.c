37bc0317dc9e52c44723766e82693ace8ea212eb
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-04-13 03:12:54 -0700

    uestion 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,12 +10,14 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int x;
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
 
-	unsigned long int x  = ~(1UL << index);
+	 x  = ~(1UL << index);
 	*n &= x;
 	return (1);
 }
