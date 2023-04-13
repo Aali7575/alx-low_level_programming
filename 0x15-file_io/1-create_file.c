0ee5fb823653ecb9df55f349f88cf0eca2696960
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-04-13 01:51:09 -0700

    fixed the betty errors now

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-int fd = open(filename , O_WRONLY | O_CREAT | O_TRUNC , S_IRUSR | S_IWUSR);
+int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
 {
 	return (-1);
@@ -22,7 +22,7 @@ if (fd == -1)
 
 if (text_content != NULL)
 {
-	ssize_t bytes= write(fd , text_content , strlen(text_content));
+	ssize_t bytes = write(fd, text_content, strlen(text_content));
 
 	if (bytes == -1)
 	{
