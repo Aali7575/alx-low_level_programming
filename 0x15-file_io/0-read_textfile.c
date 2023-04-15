d1ded18d9cd30fad6f4eca0f46cc5a6da7ece8c4
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-04-14 19:22:23 -0700

    question 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,25 +3,42 @@
 #include "main.h"
 
 /**
+ * read_textfile - reads a text file and prints the letters
  * main -  a function that reads a text file and prints it
  * to the POSIX standard output.
- *d_textfile: where letters is the number of letters
- *@filename name of file to read
- *@letters num of letters it should read and print 
+ *@filename: name of file to read
+ *@letters: num of letters it should read and print
  * Return: 0 if the file can not be opened or read
  *
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *filename;
-	int letters;
-	char buffer[100];
+	char *bufferr;
+	int filedes;
+	ssize_t x;
+	ssize_t y;
 
-	filename = fopen("read_textfile.txt", "r");
-	if (filename == NULL)
-	{
+	if (!filename)
 		return (0);
-	}
-	letters = fread(buffer, sizeof(char),100,filename);
-	
+
+	filedes = open(filename, O_RDONLY);
+
+	if (filedes == -1)
+		return (0);
+
+	bufferr = malloc(sizeof(char) * (letters));
+	if (!bufferr)
+		return (0);
+
+	x = read(filedes, bufferr, letters);
+	y = write(STDOUT_FILENO, bufferr, x);
+
+	close(filedes);
+
+	free(bufferr);
+
+	return (y);
+}
+
+
