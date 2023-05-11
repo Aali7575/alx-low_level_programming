107058b5bfcdaff7b9f16e9f15c63adefc562d2e
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-05-11 15:24:46 -0700

    question 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t writing;
-	int len = text_content ? strlen(text_content) : 0;
+	int len = strlen(text_content);
 
 	if (filename == NULL)
 	{
