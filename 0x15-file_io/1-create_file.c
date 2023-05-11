d08044ca9ef83b33b46176181d250fd32bb138b8
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-05-11 15:05:43 -0700

    changed the file permission flag

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR | O_TRUNC);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (!fd)
 	{
 		return (-1);
